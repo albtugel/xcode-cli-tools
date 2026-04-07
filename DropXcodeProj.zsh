0610546dd77466dbd176a2e3bd6301c854a486cb
Alibek Tugel <al.tugel02@gmail.com>
2026-04-08 02:07:04 +0500

    fix: add PATH for homebrew compatibility

--- DropXcodeProj.zsh
+++ DropXcodeProj.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 # ==============================================================================
 # xdrop: Hard Reset (Global DerivedData & Logs)
 # ==============================================================================
--- TestXcodeProj.zsh
+++ TestXcodeProj.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 # ==============================================================================
 # xtest: Run XCTest on iOS Simulator
 # Usage:
